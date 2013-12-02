953518101f6e6f3f51eaa18650b41472b0a4c8e6
gezichtshaar <tedteddet@gmail.com>
2013-12-02 19:33:03 +0100

    shittz

--- html/baksteen/baksteen.js
+++ html/baksteen/baksteen.js
@@ -4,12 +4,14 @@ function Baksteen (_container) {
 		kiezelsteentjes = [];
 
 	init = function () {
-		container.addEventListener("click", onClick);
+		container.addEventListener("click", onClick, true);
 	}
 	onClick = function (_event) {
-		var kiezeltje = new TekstKiezeltje(self, _event.clientX - container.offsetLeft, _event.clientY - container.offsetTop);
-		kiezelsteentjes.push(kiezeltje);
-		container.appendChild(kiezeltje.element);
+		if (_event.target === container) {
+			var kiezeltje = new TekstKiezeltje(self, _event.clientX - container.offsetLeft, _event.clientY - container.offsetTop);
+			kiezelsteentjes.push(kiezeltje);
+			container.appendChild(kiezeltje.element);
+		}
 	}
 
 	this.removeKiezeltje = function (_n) {
@@ -24,18 +26,46 @@ function Kiezeltje (_parent, _x, _y) {
 		parent = _parent,
 		x = _x,
 		y = _y,
-		element;
+		element, title, content;
 
 	init = function () {
 		element = document.createElement("div");
 		element.className = "kiezeltje";
 		element.style.left = x;
 		element.style.top = y;
+
+		element.appendChild(createTitle("Unknown"));
+		element.appendChild(createContent());
+
+		element.addEventListener("mouseover", onMouseOver, false);
+		element.addEventListener("mouseout", onMouseOut, false);
+	}
+	createTitle = function (_title) {
+		title = document.createElement("p");
+		title.className = "kiezeltje-title";
+		title.innerHTML = _title;
+		return title;
+	}
+	createContent = function () {
+		content = document.createElement("div");
+		content.className = "kiezeltje-content";
+		return content;
+	}
+	onMouseOver = function (_event) {
+		element.style.zIndex = 2;
+	}
+
+	onMouseOut = function (_event) {
+		element.style.zIndex = 1;
 	}
 
 	init();
 	return {
-		element: element
+		element: element,
+		title: title,
+		content: content,
+		update: function () {
+		}
 	};
 }
 
@@ -44,6 +74,8 @@ function TekstKiezeltje( _parent, _x, _y) {
 		parent = _parent,
 		kiezeltje = Kiezeltje(_parent, _x, _y);
 
+	kiezeltje.title.innerHTML = '<input class="kiezeltje-title" type="text"/>';
+	kiezeltje.content.innerHTML = '<textarea class="kiezeltje-content"></textarea>';
 
 	return kiezeltje;
 }
