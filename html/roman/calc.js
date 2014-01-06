3692ee12d46ddbbb38aed920777a44d35c2e5451
gezichtshaar <tedteddet@gmail.com>
2014-01-06 22:14:02 +0100

    tabs gefixt

--- html/roman/calc.js
+++ html/roman/calc.js
@@ -1,27 +1,27 @@
 var romanTranslator = (function () {
-    var translation = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+	var translation = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
 
-    function transform (_text) {
-	var numbers = [];
+	function transform (_text) {
+		var numbers = [];
 
-	for (var i = 0; i < _text.length; i++) {
-		numbers.push(translation[_text[i]]);
+		for (var i = 0; i < _text.length; i++) {
+			numbers.push(translation[_text[i]]);
+		}
+		return numbers;
+    	}
+
+	function toDec (_xs) {
+		var n = _xs.indexOf(Math.max.apply(Math, _xs));
+		return _xs.length > 0 ? (_xs[n] - arguments.callee(_xs.slice(0, n)) + arguments.callee(_xs.slice(n + 1, _xs.length))) : 0;
 	}
-	return numbers;
-    }
-
-    function toDec (_xs) {
-	var n = _xs.indexOf(Math.max.apply(Math, _xs));
-	return _xs.length > 0 ? (_xs[n] - arguments.callee(_xs.slice(0, n)) + arguments.callee(_xs.slice(n + 1, _xs.length))) : 0;
-    }
     
-    return {
-	romanToDec: function (_text) {
-	    return toDec(transform(_text));
+	return {
+		romanToDec: function (_text) {
+			return toDec(transform(_text));
+		}
 	}
-    }
 })();
 
 function calcHelper() {
-    document.getElementById("bloot").innerHTML = romanTranslator.romanToDec(document.getElementById("blaat").value);
+	document.getElementById("bloot").innerHTML = romanTranslator.romanToDec(document.getElementById("blaat").value);
 }
