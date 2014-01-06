83af698c5e34fd3e5dc6ccd071078fe269155799
gezichtshaar <pindakaas@potten.nl>
2014-01-06 22:57:25 +0100

    ooop roman

--- html/roman/calc.js
+++ html/roman/calc.js
@@ -1,24 +1,32 @@
-var input = "MCMXCIX";
-var translation = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+var romanTranslator = (function () {
+		var translation = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-function transform (_text) {
-	var numbers = [];
+		function transform (_text) {
+				var numbers = [];
 
-	for (var i = 0; i < _text.length; i++) {
-		numbers.push(translation[_text[i]]);
-	}
+				for (var i = 0; i < _text.length; i++) {
+						numbers.push(translation[_text[i]]);
+				}
+				return numbers;
+		}
 
-	return numbers;
-}
+		function toDec (_xs) {
+				var n = 0;
 
-function calc (_xs) {
-	if (_xs.length <= 0) {
-		return 0;
-	}
-	var n = _xs.indexOf(Math.max.apply(Math, _xs));
-	return _xs[n] - arguments.callee(_xs.slice(0, n)) + arguments.callee(_xs.slice(n + 1, _xs.length));
-}
+				if (_xs.length <= 0) {
+						return 0;
+				}
+
+				var n = _xs.indexOf(Math.max.apply(Math, _xs));
+				return _xs[n] - arguments.callee(_xs.slice(0, n)) + arguments.callee(_xs.slice(n + 1, _xs.length));
+		}
+		return {
+				romanToDec: function (_text) {
+						return toDec(transform(_text));
+				}
+		}
+})();
 
 function calcHelper() {
-	document.getElementById("bloot").innerHTML=calc(transform(document.getElementById("blaat").value));
+		document.getElementById("bloot").innerHTML = romanTranslator.romanToDec(document.getElementById("blaat").value);
 }
