2f4283e261f9b6fb7a92f84bb806c86376214c2b
gezichtshaar <tedteddet@gmail.com>
2014-01-06 22:11:21 +0100

    spul dingen

--- html/roman/calc.js
+++ html/roman/calc.js
@@ -1,5 +1,5 @@
 var romanTranslator = (function () {
-    var translation = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    var translation = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
 
     function transform (_text) {
 	var numbers = [];
@@ -11,15 +11,10 @@ var romanTranslator = (function () {
     }
 
     function toDec (_xs) {
-	var n = 0;
-
-	if (_xs.length <= 0) {
-		return 0;
-	}
-
 	var n = _xs.indexOf(Math.max.apply(Math, _xs));
-	return _xs[n] - arguments.callee(_xs.slice(0, n)) + arguments.callee(_xs.slice(n + 1, _xs.length));
+	return _xs.length > 0 ? (_xs[n] - arguments.callee(_xs.slice(0, n)) + arguments.callee(_xs.slice(n + 1, _xs.length))) : 0;
     }
+    
     return {
 	romanToDec: function (_text) {
 	    return toDec(transform(_text));
