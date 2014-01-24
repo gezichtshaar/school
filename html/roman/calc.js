9dc70b6e79e82a083eb6682d0f4e309ee09c98f8
gezichtshaar <pindakaas@potten.nl>
2014-01-24 22:19:23 +0100

    asd

--- html/roman/calc.js
+++ html/roman/calc.js
@@ -1,5 +1,5 @@
 var romanTranslator = (function () {
-	var translation = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+	var translation = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1};
 
 	function transform (_text) {
 		var numbers = [],
@@ -18,9 +18,19 @@ var romanTranslator = (function () {
 
 		return _xs.length > 0 ? (_xs[n] - arguments.callee(_xs.slice(0, n)) + arguments.callee(_xs.slice(n + 1, _xs.length))) : 0;
 	}
+	function toRoman (_xs, _n) {
+		if (_n > 0 && _xs.length > 0) {
+			return (_n - translation[_xs[0]] >= 0) ? (_xs[0] + arguments.callee(_xs, _n - translation[_xs[0]])) : arguments.callee(_xs.slice(1), _n);
+		}else{
+			return '';
+		}
+	}
 	return {
 		romanToDec: function (_text) {
 			return toDec(transform(_text));
+		},
+		decToRoman: function (_dec) {
+			return toRoman(Object.keys(translation), _dec);
 		}
 	}
 })();
